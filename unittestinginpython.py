252dfa5d7e005a3a69cd534ae41274807ad94639
Earl Marquise <95518992+Eqq111@users.noreply.github.com>
2022-02-27 04:59:57 -0500

    Update to UTIP
    assertTrue and assertFalse

--- unittestinginpython.py
+++ unittestinginpython.py
@@ -3,17 +3,32 @@ import unittest
 class Test(unittest.TestCase):
   
          
-    def division(numberOne, numberTwo):
-            result = numberOne/ numberTwo
-            return result
+   # def division(numberOne, numberTwo):
+   #         result = numberOne/ numberTwo
+   #         return result
 
-    def test_number_division_success(self):
-        result = Test.division(24, 12)
-        self.assertEqual(result, 2)
+   # def test_number_division_success(self):
+   #     result = Test.division(24, 12)
+   #     self.assertEqual(result, 2)
     
-    def test_number_division_different_number(self):
-        result = Test.division(24, 12)
-        self.assertNotEqual(result, 3)
+   # def test_number_division_different_number(self):
+   #     result = Test.division(24, 12)
+   #     self.assertNotEqual(result, 3)
+
+    def is_empty(word):
+        if(len(word)==0):
+            return True
+        else:
+            return False
+          
+    def test_is_empty_string_successful(self):
+        result = Test.is_empty("")
+        self.assertTrue(result, True)
+        
+    def test_is_empty_with_string_successful(self):
+        result = Test.is_empty("")
+        self.assertFalse(result, True)
+ 
 
 
 if __name__ == '__main__' :
